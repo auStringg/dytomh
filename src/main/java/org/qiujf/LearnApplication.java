5115f2da7bc18efc3ae2e4029248dd5b26fb9388
dytomh <dytomh@qq.com>
2022-04-29 17:48:19 +0800

    添加心率目录，引入springboot和mybatisplus todo 自动生成实体类

--- src/main/java/org/qiujf/LearnApplication.java
+++ src/main/java/org/qiujf/LearnApplication.java
@@ -1,10 +1,12 @@
 package org.qiujf;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.qiujf.entity.mapper")
 public class LearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
